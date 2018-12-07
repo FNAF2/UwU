964a254ec2cf0bc10ac46e5d6030800315c9fa50
springfoxteam <31635846+springfoxteam@users.noreply.github.com>
2018-12-06 19:38:38 -0500

    Yes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js');
-const Bot = new Discord.Client();
+const Commando = require('discord.js-commando');
+const Bot = new Commando.Client();
 const Key = 'NTIwMzc0NjMwNDQ4NDk2NjQw.Dus_bQ.QZk9UFwA6w07-299NPBZ069rQw0'
 
 Bot.on('message', function(message){
     if(message.content == 't!test')
     {
-        message.reply('Testing Connection');
+        message.channel.sendMessage('Testing Connection' + message.author);
         console.log("Command TEST sent")
     }
 });
