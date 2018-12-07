48cdbb04c36e12b71c7a0368ab19e15316d04d3d
springfoxteam <31635846+springfoxteam@users.noreply.github.com>
2018-12-06 20:02:22 -0500

    A

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const Commando = require('discord.js-commando');
-const Bot = new Commando.Client();
+const Bot = new Commando.Client({
+    commandPrefix: 't!'
+    });
 const Key = 'NTIwMzc0NjMwNDQ4NDk2NjQw.Dus_bQ.QZk9UFwA6w07-299NPBZ069rQw0'
+
+bot.user.setPresence({ status: 'online', game: { name: 'type t!help' } });
+
+Bot.registry.registerGroup('simple', 'Simple');
+Bot.registry.registerDefaults();
+Bot.registry.registerCommandsIn(__dirname + "/commands");
 
 Bot.on('message', function(message){
     if(message.content == 't!test')
